12a477eeac050a496cb9f36502d3e4404b1426a6
lyltt51 <1761519840@qq.com>
2022-06-27 18:00:45 +0800

    tabcontrol

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="tab-control">
+    <div class="tab-control-item" :key="index" v-for="(item,index) in titles" :class="{active: index === currentIndex}">
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabControl',
+  props: {
+    titles: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  data() {
+    return {
+      currentIndex: 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .tab-control {
+    display: flex;
+    text-align: center;
+    font-size: 15px;
+    height: 40px;
+    line-height: 40px;
+  }
+
+  .tab-control-item {
+    flex: 1;
+  }
+
+  .tab-control-item span {
+    padding: 5px;
+  }
+
+  .active {
+    color: var(--color-tint);
+  }
+
+  .active span {
+    border-bottom: 3px solid var(--color-tint);
+  }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,22 +3,72 @@
     <Navbar class="home-nav"><div slot="center">购物街</div></Navbar>
     <home-swipper :banners="banners"></home-swipper>
     <recommend-view :recommends="recommends"></recommend-view>
+    <feture-view></feture-view>
+    <tab-control :titles="['流行','新款','精选']"></tab-control>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>0</li>
+      <li>-</li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
   </div>
 </template>
 
 <script>
-import Navbar from 'components/common/navbar/Navbar.vue'
 import HomeSwipper from './childComp/HomeSwipper.vue'
 import RecommendView from './childComp/RecommendView.vue'
+import FetureView from './childComp/FetureView.vue'
+
+import Navbar from 'components/common/navbar/Navbar.vue'
+import TabControl from 'components/content/tabControl/TabControl'
 
 import { getHomeMultidata } from 'network/home.js'
 
 export default {
   name: "MyHome",
   components: {
-    Navbar,
     HomeSwipper,
-    RecommendView
+    RecommendView,
+    FetureView,
+    Navbar,
+    TabControl,
+    TabControl
   },
   data () {
     return {
@@ -37,7 +87,16 @@ export default {
 
 <style scoped>
   .home-nav {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
     background-color: var(--color-tint);
     color: #fff;
+    z-index: 9;
+  }
+
+  #home {
+    padding-top: 44px;
   }
 </style>
